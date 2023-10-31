a4161453acac3a588c012a75c76f83d28cd654dc
Sam <99977535+shemkumar@users.noreply.github.com>
2023-10-31 18:49:25 +0530

    add user name password field

--- wifi1.py
+++ wifi1.py
@@ -9,11 +9,14 @@ driver.get("#ipadress of wifi login page")
 username = WebDriverWait(driver, 10).until(
     EC.element_to_be_clickable((By.ID, "username"))
 )
-username.send_keys("#username")
+#enter wifi user name
+username.send_keys("")
 password = WebDriverWait(driver, 10).until(
     EC.element_to_be_clickable((By.ID, "password"))
 )
-password.send_keys("pass")
+
+#enter wifi password
+password.send_keys("")
 login_button = WebDriverWait(driver, 10).until(
     EC.element_to_be_clickable((By.ID, "loginbutton"))
 )
